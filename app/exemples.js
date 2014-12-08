bf34a5295b680d61e352c49ce95cdb18c43b580a
Aleksandr Malakhanov <aleksandr.malakhanov@customertimes.com>
2014-12-08 16:10:07 +0200

    mod1

--- app/exemples.js
+++ app/exemples.js
@@ -22,14 +22,21 @@ angular.module("regexp.exemples", []).constant("Exemples", {
       {type: "match", input: "3", regexp: "/\\d/"}
     ]
   },
+  "/anchors": {
+    title: "Якоря",
+    components: [
+      {type: "match", title: "Поиск с опором на конец строки", input: "123, 456", regexp: "/\\d+$/"},
+      {type: "match", title: "Сопоставление строки целиком", input: "123, 456", regexp: "/^\\d+$/"},
+    ]
+  },
   "/quantors": {
     title: "Кванторы",
     components: [
-      {type: "match", input: "12354", regexp: "/\\d{5}/"},
-      {type: "match", input: "12354", regexp: "/\\d{4}/"},
-      {type: "match", input: "123", regexp: "/\\d{2,4}/"},
-      {type: "match", input: "aaaa", regexp: "/\\a*/"},
-      {type: "match", input: "", regexp: "/a+/"}
+      {type: "match", title: "Сопоставлеине с числом из <b>5-ти</b> цифр", input: "12354", regexp: "/^\\d{5}$/"},
+      {type: "match", title: "Сопоставлеине с числом из <b>4-х</b> цифр", input: "12354", regexp: "/^\\d{4}$/"},
+      {type: "match", title: "Сопоставлеине с числом от <b>2-х до 4-х</b> цифр", input: "123", regexp: "/^\\d{2,4}$/"},
+      {type: "match", title: "Поиск <b>0 или более</b> символов \"a\"", input: "aaaa", regexp: "/\\a*/"},
+      {type: "match", title: "Поиск <b>1 или более</b> символов \"a\"", input: "", regexp: "/a+/"}
     ]
   },
   "/groups": {
@@ -41,13 +48,6 @@ angular.module("regexp.exemples", []).constant("Exemples", {
       {type: "match", title: "Значения, захваченные в группы также можно использовать в самом регулярном вырежении как <code>\\номер_группы</code>", input: "#333", regexp: "/#([0-9a-f])\\1\\1/"},
     ]
   },
-  "/anchors": {
-    title: "Якоря",
-    components: [
-      {type: "match", title: "Поиск с опором на конец строки", input: "123, 456", regexp: "/\\d+$/"},
-      {type: "match", title: "Сопоставление строки целиком", input: "123, 456", regexp: "/^\\d+$/"},
-    ]
-  },
   "/modifiers": {
     title: "Модификаторы",
     components: [
@@ -56,28 +56,43 @@ angular.module("regexp.exemples", []).constant("Exemples", {
       {type: "match", title: "Многострочный поиск - модификатор <code>m</code>", input: "Hello, world.\nПривет, мир.", regexp: "/^(\\w+), (\\w+)\\.$/m", multiline: true},
     ]
   },
-  
-  "/classes1": {
-    title: "Символьные классы",
+  "/greedy": {
+    title: "Жадность и лень",
     components: [
-      {
-        title: "Например, есть класс «произвольная цифра», обозначается \d. Пример ниже ищет любую цифру в строке:",
-        type: "replace", regexp: "//g", replace: "$1", input: "12345678901"},
-      {
-        type: "match",
-        title: "adsasd",
-        regexp: "/qw\\w/",
-        input: "qwe",
-        tests: [
-          {input: "qwe", expected: true},
-          {input: "qwD", expected: true},
-          {input: "Dwe", expected: false},
-          {input: "asd", expected: false}
-        ]
-      },
-      {type: "match", regexp: "/123/", input: "321"},
+      {type: "match", title: "Поиск greedy-оператором <code>*</code>", input: "a = 1; b = 2;", regexp: "/.*;/g"},
+      {type: "match", title: "Поиск lazy-оператором <code>*?</code>", input: "a = 1; b = 2;", regexp: "/.*?;/g"},
+    ]
+  },
+  "/statements": {
+    title: "Утверждения",
+    components: [
+      {type: "match", title: "Поиск \"пассивной группой\" смещает указатель, за счет чего после проверки первой части проверяется вторая. Но при этом захвата в группу не происходит.", input: "a = 1; b = 2;", regexp: "/(?:\\w = \\d; ?)*/"},
+      {type: "match", title: "Поиск \"просмотром вперед\" <u>не</u> смещает указатель, за счет чего после проверки первой части производится проверка первой повторно. Захвата в группу не происходит.", input: "a = 1; b = 2;", regexp: "/(?=\\w = \\d; ?)*/"},
+      {type: "match", title: "Поиск \"просмотром вперед\" <u>не</u> смещает указатель, за счет чего после проверки первой части производится проверка первой повторно. Захвата в группу не происходит.", input: "<div><b></b><i></i></div>", regexp: "/<\\/?(?!div).+?>/g", multiline: true},
     ]
   }
+  
+//  "/classes1": {
+//    title: "Символьные классы",
+//    components: [
+//      {
+//        title: "Например, есть класс «произвольная цифра», обозначается \d. Пример ниже ищет любую цифру в строке:",
+//        type: "replace", regexp: "//g", replace: "$1", input: "12345678901"},
+//      {
+//        type: "match",
+//        title: "adsasd",
+//        regexp: "/qw\\w/",
+//        input: "qwe",
+//        tests: [
+//          {input: "qwe", expected: true},
+//          {input: "qwD", expected: true},
+//          {input: "Dwe", expected: false},
+//          {input: "asd", expected: false}
+//        ]
+//      },
+//      {type: "match", regexp: "/123/", input: "321"},
+//    ]
+//  }
 }).config(function (Exemples) {
   for (var i in Exemples) {
     for (var j in Exemples[i].components) {
